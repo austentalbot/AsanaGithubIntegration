44e8fabff20cf5a86ef57236fc6773e4e7b6b960
Austen Talbot <austen@asana.com>
2014-11-03 13:00:06 -0800

    Set up test post to asana api to create task

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //server related dependencies
 var express = require('express');
-var port = process.env.PORT || 4545;
-var cors=require('cors');
+var cors = require('cors');
 var bodyParser = require('body-parser');
+var request = require('request');
+
+var port = process.env.PORT || 4545;
+var asana = 'https://app.asana.com/api/1.0'
 
 //load credentials locally or from host
 var credentials = {};
@@ -14,13 +17,62 @@ if (process.env.PORT===undefined) {
   };
 }
 
+console.log(credentials);
+
 //initialize app and use cors & body parser
 var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//create functions to be used for endpoints
+var createTask = function(req) {
+  //sent post request to asana api endpoint '/tasks'
+  //include data block
+
+  // 'https://app.asana.com/api/1.0/users/me'
+
+  var apiKey = new Buffer(credentials.key + ':').toString('base64');
+  console.log(apiKey);
+
+  // request.get('https://app.asana.com/api/1.0/users/me', {
+  //   'auth': {
+  //     'user': credentials.key,
+  //     'pass': '',
+  //     'sendImmediately': true
+  //   }
+  // }, function(err, resp, body) {
+  //   console.log(err);
+  //   console.log(body);
+  // });
+
+  request.post({
+    url: asana + '/tasks',
+    auth: {
+      'user': credentials.key,
+      'pass': '',
+      'sendImmediately': true
+    },
+    form: {
+      api_key: apiKey,
+      assignee: credentials.assignee,
+      name: 'Test task',
+      workspace: credentials.workspace,
+      project: credentials.project
+    }
+  }, function(err, resp, body) {
+    console.log(err);
+    console.log(body);
+  });
+
+};
+
 app.get('/', function(req, res){
+  res.status(200).send('Asana Github Integration');
+});
+
+app.get('/test', function(req, res){
+  createTask();
   res.status(200).send('Asana Github Integration');
 });
 
