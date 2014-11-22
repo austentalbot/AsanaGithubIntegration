68d95f2461bc31bbba2c53ec48165072c79e3f60
Austen Talbot <austen@asana.com>
2014-11-21 17:32:57 -0800

    Adding interactivity to show when user is added

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var React = require('react');
+var React = require('react/addons');
 window.React = React;
 var r = React.createElement;
 
--- client/components/AddUser.js
+++ client/components/AddUser.js
@@ -1,8 +1,9 @@
 var reqwest = require('reqwest');
 
-var React = require('react');
+var React = require('react/addons');
 window.React = React;
 var r = React.createElement;
+var cx = React.addons.classSet;
 
 var AddUser = React.createClass({
   getInitialState: function() {
@@ -10,7 +11,39 @@ var AddUser = React.createClass({
       added: false,
       error: false
     }
-  }
+  },
+  addUser: function() {
+    var that = this;
+    var asana = document.getElementById('asanaInput').value;
+    var github = document.getElementById('githubInput').value;
+    if (!asana || !github) {
+      alert('You must fill out both fields');
+      return;
+    }
+    reqwest({
+      url: 'http://127.0.0.1:4545/addUser',
+      method: 'post',
+      data: {
+        github: github,
+        asana: asana
+      },
+      error: function(err) {
+        console.log(err);
+      },
+      success: function (resp) {
+        console.log(resp);
+        document.getElementById('asanaInput').value = '';
+        document.getElementById('githubInput').value = '';
+        that.setState({added: true});
+      }
+    });
+  },
+  onInputChange: function(e) {
+    this.setState({
+        added: e.target.value.length === 0 && this.state.added,
+        error: e.target.value.length === 0 && this.state.error
+    });
+  },
   render: function() {
     return r('div', {
       children: [
@@ -18,41 +51,21 @@ var AddUser = React.createClass({
         r('div', {}, 'Asana id:'),
         r('input', {
           type: 'text',
-          id: 'asanaInput'
+          id: 'asanaInput',
+          onChange: this.onInputChange
         }),
         r('div', {}, 'Github handle:'),
         r('input', {
           type: 'text',
-          id: 'githubInput'
+          id: 'githubInput',
+          onChange: this.onInputChange
         }),
         r('button', {
           className: 'button button--sm',
-          onClick: function() {
-            var asana = document.getElementById('asanaInput').value;
-            var github = document.getElementById('githubInput').value;
-            if (!asana || !github) {
-              console.log('You must fill out both fields');
-              return;
-            }
-            reqwest({
-              url: 'http://127.0.0.1:4545/addUser',
-              method: 'post',
-              data: {
-                github: github,
-                asana: asana
-              },
-              error: function(err) {
-                console.log(err);
-              },
-              success: function (resp) {
-                console.log(resp);
-                this.setState({added: true});
-              }
-            });
-          }
+          onClick: this.addUser
         }, 'Submit'),
         r('h1', {
-          className: 'hidden'
+          className: cx({'hidden': !this.state.added})
         }, 'Added new user!')
       ]
     });
